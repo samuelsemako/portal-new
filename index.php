e1fb36e47ea89edf0a69a3179bbef1aa906842cd
samuelsemako <emmasamsemaco2001@gmail.com>
2024-05-05 16:52:41 +0100

    First Upload of this solution 5/5/24

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "meta.php"?>
+</head>
+<body>
+    <section class= "body-div">
+        <div class="body-div-in">
+            <div class="side-bar-div">
+                <div class="name-div">
+                    <div class="name">
+                        <h1>ututorial</h1>
+                        <i class="bi bi-toggle-on"></i>
+                    </div>
+                    
+
+
+                    <div class="search-div">
+                        <i class="bi bi-search"></i> 
+                        <span>Search</span>
+                    </div>
+                    
+                </div>
+
+
+                <div class="menu-list">
+                    <ul>
+                        <li> <i class="bi bi-pie-chart"></i>Overview</li>
+                        <li> <i class="bi bi-calendar"></i>Schedule</li>
+                        <li> <i class="bi bi-book"></i>Courses</li>
+                        <li> <i class="bi bi-bell"></i>Materials</li>
+                        <li> <i class="bi bi-gear"></i>Settings</li>
+                    </ul>
+                    
+                </div>
+                
+
+
+
+            </div>
+
+
+            <div class="body-content-div">
+                <div class="header">
+                    <div class="left">
+                        <h3>Overview</h3>
+                    </div>
+
+                    <div class="right">
+                        <div class="right-in">
+                            <i class="bi bi-bell"></i>
+                            <div class="profile">
+                                <div class="image-div">
+                                    <img src="all-images/semako.jpg" alt="">
+                                </div>
+                                <span>Semako <i class="bi bi-caret-down-fill"></i></span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+
+
+
+
+                <div class="achievement-div">
+                    <div class="achievement-div-in">
+                        <div class="achieve-cont-div">
+                            <i class="bi bi-arrow-clockwise"></i>
+                            <div class="text-div">
+                                <span>In progress</span>
+                                <h4>4 lessons</h4>
+                            </div>
+                        </div>
+
+                        <div class="achieve-cont-div">
+                            <i class="bi bi-check2-circle check"></i>
+                            <div class="text-div">
+                                <span>Completed</span>
+                                <h4>18 lessons</h4>
+                            </div>
+                        </div>
+
+                        <div class="achieve-cont-div">
+                            <i class="bi bi-clock clock"></i>
+                            <div class="text-div">
+                                <span>Education Time</span>
+                                <h4>13h 10m</h4>
+                            </div>
+                        </div>
+
+                        <div class="achieve-cont-div last">
+                            <i class="bi bi-arrow-clockwise arrow"></i>
+                            <div class="text-div">
+                                <span>achievement</span>
+                                <h4>11</h4>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+                <div class="recommended-lesson-div">
+                    <div class="recomended-div">
+                        <div class="headline">
+                            <h4>Recommended</h4>
+                            <span>See all <i class="bi bi-arrow-right"></i></span>
+                        </div>
+
+                        <div class="rec-cont-div">
+                            <div class="image-div">
+                                
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="lesson-div">
+                        hgfds
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ meta.php
@@ -0,0 +1,18 @@
+
+<meta charset="UTF-8">
+<link rel="shortcut icon" type="image-png" href="all-images/image-pix/icon.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="ROBOTS" content="ALL">
+<meta name="Engine" content="all">
+<meta name="distribution" content="global">
+
+<link href="styles/icons-1.10.2/font/bootstrap-icons.css" type="text/css" rel="stylesheet" >
+<link href="styles/style.css" type="text/css" rel="stylesheet" />
+<link href="styles/animate.css" type="text/css" rel="stylesheet" />
+<link href="styles/aos.css" type="text/css" rel="stylesheet" />
+
+<script src="js/aos.js"></script>
+<script src="js/jquery-v3.6.1.min.js"></script>
+
+<script src="js/superplaceholder.js"></script> 
